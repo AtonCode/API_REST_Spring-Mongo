8b205af9e826267afa3ffbe18d565a33929a361a
Alejandro Sacristan Leal <alejandro-sacristan@javeriana.edu.co>
2022-08-02 20:58:33 -0500

    v

--- src/main/java/me/atoncode/api_rest/ApiRestApplication.java
+++ src/main/java/me/atoncode/api_rest/ApiRestApplication.java
@@ -3,11 +3,6 @@ package me.atoncode.api_rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 @SpringBootApplication
 public class ApiRestApplication {
 
